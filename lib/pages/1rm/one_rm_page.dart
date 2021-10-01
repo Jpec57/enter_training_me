8ed7a25f47d17044777d270d13b06251aa673684
Jean-Paul Bella <jean-paul.bella@moltomedia.de>
2021-10-01 11:22:50 +0200

    Perfect

--- lib/pages/1rm/one_rm_page.dart
+++ lib/pages/1rm/one_rm_page.dart
@@ -1,4 +1,5 @@
 import 'package:enter_training_me/custom_theme.dart';
+import 'package:enter_training_me/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
@@ -11,7 +12,9 @@ class OneRMPage extends StatefulWidget {
 }
 
 class _OneRMPageState extends State<OneRMPage> {
-  double maxWeight = 100;
+  double givenWeight = 100;
+  int _selectedRepNumber = 1;
+
   late TextEditingController _textEditingController;
   final List<double> matchingPercents = [
     1,
@@ -40,54 +43,26 @@ class _OneRMPageState extends State<OneRMPage> {
   void initState() {
     _textEditingController = TextEditingController();
     _textEditingController.addListener(() {
-      setState(() {
-        maxWeight = double.parse(_textEditingController.text);
-      });
+      if (_textEditingController.text.isNotEmpty) {
+        setState(() {
+          givenWeight = double.parse(_textEditingController.text);
+        });
+      }
     });
     super.initState();
   }
 
+  double _calculateOneRM() {
+    return givenWeight / matchingPercents[_selectedRepNumber - 1];
+  }
+
   List<Widget> _renderGridChildren() {
-    List<Widget> children = [
-      Container(
-        color: Colors.red,
-        child: Row(
-          children: const [
-            Expanded(
-              child: Center(
-                child: Text("Reps",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text("Percent",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text("Weight",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
-            ),
-          ],
-        ),
-      )
-    ];
+    List<Widget> children = [];
     for (int i = 0; i < matchingPercents.length; i++) {
       int reps = i + 1;
       double percent = matchingPercents[i];
-      double weight = percent * maxWeight;
+
+      double weight = percent * _calculateOneRM();
       children.add(Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0),
         child: Row(
@@ -99,7 +74,7 @@ class _OneRMPageState extends State<OneRMPage> {
               child: Center(child: Text((percent * 100).toInt().toString())),
             ),
             Expanded(
-              child: Center(child: Text(weight.toString() + " kg")),
+              child: Center(child: Text(weight.toStringAsFixed(1) + " kg")),
             ),
           ],
         ),
@@ -118,6 +93,8 @@ class _OneRMPageState extends State<OneRMPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
+      drawer: const MyDrawer(),
       backgroundColor: CustomTheme.darkGrey,
       body: SafeArea(
         child: Padding(
@@ -127,29 +104,107 @@ class _OneRMPageState extends State<OneRMPage> {
               Row(
                 children: [
                   Flexible(
-                    child: TextField(
-                      controller: _textEditingController,
-                      keyboardType: TextInputType.number,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "100",
-                          hintStyle: TextStyle(color: Colors.white60)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: TextField(
+                        controller: _textEditingController,
+                        keyboardType: TextInputType.number,
+                        style: const TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)),
+                            hintText: "100",
+                            hintStyle: TextStyle(color: Colors.white60)),
+                      ),
                     ),
                   ),
+                  Theme(
+                    data: Theme.of(context).copyWith(
+                      canvasColor: CustomTheme.darkGrey,
+                    ),
+                    child: DropdownButton<int>(
+                      value: _selectedRepNumber,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.white),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.white,
+                      ),
+                      onChanged: (int? newValue) {
+                        setState(() {
+                          _selectedRepNumber = newValue!;
+                        });
+                      },
+                      items: <int>[
+                        ...List.generate(
+                            matchingPercents.length, (index) => index + 1)
+                      ].map<DropdownMenuItem<int>>((int value) {
+                        return DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(
+                            "${value}RM",
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  )
                 ],
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 32.0),
-                child: Text("Your 1RM is probably $maxWeight kg"),
+                child: Text(
+                    "Your 1RM is probably ${_calculateOneRM().toStringAsFixed(1)} kg"),
               ),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: SingleChildScrollView(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: _renderGridChildren())),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        color: Colors.red,
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        child: Row(
+                          children: const [
+                            Expanded(
+                              child: Center(
+                                child: Text("Reps",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                            ),
+                            Expanded(
+                              child: Center(
+                                child: Text("Percent",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                            ),
+                            Expanded(
+                              child: Center(
+                                child: Text("Weight",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: SingleChildScrollView(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: _renderGridChildren())),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
